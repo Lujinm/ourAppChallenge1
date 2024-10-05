6df9e6932ff38ff245ee64a7055c2a8e318cf59a
nora <norahi.braheeem@gmail.com>
2024-10-05 20:05:10 +0300

    new changes

--- norahi.swift
+++ norahi.swift
@@ -1,105 +1,357 @@
-//
-//  norahi.swift
-//  ourAppChallenge1
-//
-//  Created by nora on 27/03/1446 AH.
-//
-
 import SwiftUI
 import Charts
+
 struct norahi: View {
+    @State private var isChecked = false //for the check
+
     var body: some View {
         VStack() {
-            Spacer()
             
-            /*Image(systemName: "arrow.backward")
-                .renderingMode(.original)
-                .aspectRatio(contentMode: .fill)
-                .padding(.trailing, 350.0)*/
             
             Text("The progress")
                 .font(.largeTitle)
                 .fontWeight(.bold)
                 .padding(.trailing, 150.0)
                 .padding(.top, 100.0)
-            
-            RoundedRectangle(cornerRadius: 25)
-                .fill(Color.accent)
-                .padding(.bottom, 12.29)
-                .shadow(radius: 3)
-            
-                .frame(width: 370, height: 250)
-            
+            ZStack{
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(Color.accent)
+                    .padding(.bottom, 12.29)
+                    .shadow(radius: 3)
+                    .frame(width: 370, height: 250)
+                
+                VStack{
+                    
+                    
+                    Text("name")
+                        .padding(.trailing, 50.0)
+                        
+                    
+                   
+                        
+                        
+                        
+                    
+                    Text("cat state")
+                        .padding(.trailing, 240.0)
+                        .padding(.bottom, 10.0)
+                        
+                    Text("progress %")
+                        .padding(.trailing, 240.0)
+                        
+                    
+                    
+                    
+                }
+            }
             Spacer()
-            ZStack{
-                HStack{
-                    //icon
-                    VStack{
-                        //food
-                    }
-                    //button
-                }
-            }
-            ZStack{
-                HStack{
-                    //icon
-                    VStack{
-                        //hydration
-                    }
-                    //button
-                }
-            }
-            ZStack{
-                HStack{
-                    //icon
-                    VStack{
-                        //hydration
-                    }
-                    //button
-                }
-            }
-                
-                
-                
+            
+            ZStack {
                 RoundedRectangle(cornerRadius: 20)
                     .fill(Color.accent)
-                
                     .frame(width: 370, height: 90)
                     .presentationCornerRadius(300)
                     .shadow(radius: 3)
                 
+                HStack {
+                    Image(systemName: "sun.min")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .padding(.leading, 45.0)
+                        .frame(width: 40, height: 40)
+                    
+                    
+                    
+                    Text("First meal")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("chart 1st color"))
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading, 30.0)
+                        .padding(.bottom, 29.0)
+                    
+                    
+                    
+                    Spacer()
+                    Button(action: {
+                        isChecked.toggle()
+                    }) {
+                        Image(systemName: isChecked ? "checkmark.circle.fill" : "circle")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 40, height: 40) //
+                            .foregroundColor(isChecked ? Color(hex: "F24405") : .black)
+                            .padding(25)
+                    }
+                }
                 
-                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/20.0/*@END_MENU_TOKEN@*/)
-                    .fill(Color.accentColor2)
-                    .frame(width: 370, height: 90)
-                    .presentationCornerRadius(309)
-                    .shadow(radius: 3)
+                VStack{
+                    Text("Grams amount")
+                    
+                        .padding(.top, 30)
+                        .padding(.trailing, 135.0)
+                    
+                    
+                }
                 
                 
-                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/20.0/*@END_MENU_TOKEN@*/)
-                    .fill(Color.accent)
-                    .frame(width: 370, height: 90)
-                    .presentationCornerRadius(309)
-                    .shadow(radius: 3)
-                
-                
-                
-                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/20.0/*@END_MENU_TOKEN@*/)
-                    .fill(Color.accent)
-                    .frame(width: 370, height: 90)
-                    .presentationCornerRadius(309)
-                    .shadow(radius: 3)
-                
-                
-                
-                
-                
-                
+                .padding()
             }
+            //fist meal
+            
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color.accent)
+                    .frame(width: 370, height: 90)
+                    .presentationCornerRadius(300)
+                    .shadow(radius: 3)
+                
+                HStack {
+                    Image(systemName: "sun.and.horizon")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .padding(.leading, 60.0)
+                        .frame(width: 30, height: 30)
+                    
+                    
+                    
+                    Text("Second meal")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("chart 1st color"))
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading, 39.0)
+                        .padding(.bottom, 29.0)
+                    
+                    
+                    
+                    Spacer()
+                    Button(action: {
+                        isChecked.toggle()
+                    }) {
+                        Image(systemName: isChecked ? "checkmark.circle.fill" : "circle")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 40, height: 40)                             .foregroundColor(isChecked ? Color(hex: "F24405") : .black)
+                            .padding(25)
+                    }
+                }
+                
+                VStack{
+                    Text("Grams amount")
+                    
+                        .padding(.top, 30)
+                        .padding(.trailing, 129.0)
+                    
+                    
+                }
+                
+                
+                .padding()
+            }
+            //second meal
+            
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color.accent)
+                    .frame(width: 370, height: 90)
+                    .presentationCornerRadius(300)
+                    .shadow(radius: 3)
+                
+                HStack {
+                    Image(systemName: "moon.stars.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .foregroundColor(Color.black)
+                        .padding(.leading, 60.0)
+                        .frame(width: 40, height: 40)
+                    
+                    
+                    
+                    Text("Third meal")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("chart 1st color"))
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading, 30.0)
+                        .padding(.bottom, 29.0)
+                        
+                    
+                    
+                    
+                    Spacer()
+                    Button(action: {
+                        isChecked.toggle()
+                    }) {
+                        Image(systemName: isChecked ? "checkmark.circle.fill" : "circle")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 40, height: 40) // تحديد حجم الزر
+                            .foregroundColor(isChecked ? Color(hex: "F46B07") : .black)
+                            .padding(25)
+                    }
+                }
+                
+               
+                VStack{
+                    Text("Grams amount")
+                    
+                        .padding(.top, 30)
+                        .padding(.trailing, 129.0)
+                    
+                    
+                }
+                
+                .padding()
+            }
+            //third meal
+            
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color.accent)
+                    .frame(width: 370, height: 90)
+                    .presentationCornerRadius(300)
+                    .shadow(radius: 3)
+                
+                HStack {
+                    Image(systemName: "waterbottle.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .padding(.leading, 60.0)
+                        .frame(width: 40, height: 40)
+                    
+                    
+                    
+                    Text("Hydration")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("chart 2nd color 1"))
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading, 39.0)
+                        
+                    
+                    
+                    
+                    Spacer()
+                    Button(action: {
+                        isChecked.toggle()
+                    }) {
+                        Image(systemName: isChecked ? "checkmark.circle.fill" : "circle")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 40, height: 40) // تحديد حجم الزر
+                            .foregroundColor(isChecked ? Color(hex: "F46B07") : .black)
+                            .padding(25)
+                    }
+                }
+                
+                VStack{
+                    Text("")
+                    
+                        .padding(.top, 30)
+                        .padding(.trailing, 129.0)
+                    
+                    
+                }
+                
+                
+                .padding()
+            }
+            //hydration
+            
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color.accent)
+                    .frame(width: 370, height: 90)
+                    .presentationCornerRadius(300)
+                    .shadow(radius: 3)
+                
+                HStack {
+                    Image(systemName: "progress.indicator")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .padding(.leading, 60.0)
+                        .frame(width: 40, height: 40)
+                    
+                    
+                    
+                    Text("Activity")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("chart 3rd color 2"))
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading, 39.0)
+                        
+                    
+                    
+                    
+                    Spacer()
+                    Button(action: {
+                        isChecked.toggle()
+                    }) {
+                        Image(systemName: isChecked ? "checkmark.circle.fill" : "circle")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 40, height: 40)                              .foregroundColor(isChecked ? Color(hex: "F28705") : .black)
+                            .padding(25)
+                    }
+                }
+                
+                VStack{
+                    Text("")
+                    
+                        .padding(.top, 30)
+                        .padding(.trailing, 129.0)
+                    
+                    
+                }
+                
+                
+                .padding()
+            }
+            //activity
+            
+            
+            
+            
+            
+            
+            
         }
-       
+        
+    
+            
+           
+        
+        
+        
+        
+        
+        
     }
+}
 
+
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        scanner.currentIndex = scanner.string.startIndex
+
+        var rgb: UInt64 = 0
+        scanner.scanHexInt64(&rgb)
+
+        let red = Double((rgb & 0xFF0000) >> 16) / 255.0
+        let green = Double((rgb & 0x00FF00) >> 8) / 255.0
+        let blue = Double(rgb & 0x0000FF) / 255.0
+
+        self.init(red: red, green: green, blue: blue)
+    }
+}
 
 #Preview {
     norahi()
