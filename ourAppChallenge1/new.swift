93775395c424d228b900de501d5e7b599eb72d12
lujin <lujinmohammed223@gmail.com>
2024-10-07 15:55:32 +0300

    update4

---
+++ ourAppChallenge1/new.swift
@@ -0,0 +1,46 @@
+//
+//  new.swift
+//  ourAppChallenge1
+//
+//  Created by nora on 30/03/1446 AH.
+//
+
+import SwiftUI
+
+struct new: View {
+    var body: some View {
+        ZStack{
+            Spacer()
+            VStack{
+                
+                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
+                    .frame(width:350, height:170)
+                    .padding(.bottom, 550.0)
+                //progress with the chart
+                
+                
+                
+                
+                
+                
+                
+            }
+            
+            
+            
+            
+            
+            
+    
+            
+            
+        }
+        
+        
+        
+    }
+}
+
+#Preview {
+    new()
+}
